af3ae3253eeed947f98b92c75d34e352df50e434
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-13 15:17:41 +1100

    create documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Must have name and price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   // res.status(200).json({
@@ -33,15 +23,24 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+  // const newTour = await Tour.create(req.body);
+  // console.log(newTour);
 };
 
 exports.updateTour = (req, res) => {};

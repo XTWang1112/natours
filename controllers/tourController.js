dabb654be57017cebb367644d9d3a719b5154008
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-15 14:36:06 +1100

    making the api better by sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,7 +11,16 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      // sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // execute the query
     const tours = await query;

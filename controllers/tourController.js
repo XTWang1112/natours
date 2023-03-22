6123ecffa18d645ad374c0b1fbc489b124c4af3d
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-22 14:41:11 +1100

    data validation: built-in validators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,12 +73,12 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true,
     });
     res.status(200).json({
       status: 'success',
       data: {
         tour: tour,
-        runValidators: true,
       },
     });
   } catch (err) {

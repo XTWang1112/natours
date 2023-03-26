ba628de2d69c9119bc1c32fe9b8655e80c195e02
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-27 00:01:38 +1100

    handling invalid database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -22,6 +24,11 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -29,6 +36,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = Object.assign(err);
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(err);
+    }
+    sendErrorProd(error, res);
   }
 };

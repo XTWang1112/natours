5422e7c4f1c3382e60a8f5ac37c2e3631d24ee5b
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-29 14:52:19 +1100

    logging in users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const jwt = require('jsonwebtoken');
+const AppError = require('../utils/appError');
+
+const signToken = (id) =>
+  jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRED_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   // The following line of code will cause serious flaw
@@ -12,15 +18,34 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRED_IN,
-  });
-
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: 'success',
     token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // check if user exists && password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // if everything is ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token: token,
   });
 });

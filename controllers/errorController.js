128d286136dfaf182b221afa12a405b57760f917
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-03-30 22:47:09 +1100

    protecting tour routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -41,6 +41,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = (err) =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = (err) =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -53,6 +59,9 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFiledsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
 
     sendErrorProd(error, res);
   }
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -5,15 +5,12 @@ class APIFeatures {
   }
 
   filter() {
-    console.log('filter');
-    console.log(this.queryString);
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
     this.query = this.query.find(JSON.parse(queryStr));
 
     return this;

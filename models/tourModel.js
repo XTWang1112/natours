3e327926a8b5af28bcd4cc9b8b29b46f988c9407
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-04-20 22:27:24 +1000

    embed tour guide in tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -98,6 +99,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -112,6 +114,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 //document middleware: runs before .save() and .create(), but not .insertMany()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 

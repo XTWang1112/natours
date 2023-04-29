e6917953da9a217b3484f9bfccc7fd3bc78ed161
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-04-29 19:15:53 +1000

    calculate distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -164,11 +164,11 @@ tourSchema.pre('/^find/', function (next) {
 });
 
 // Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 

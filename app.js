878119f21c3e8047b1c4be4dfb3bdafcfc11a5cd
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-04-30 12:25:05 +1000

    pub fundimentals

--- app.js
+++ app.js
@@ -66,8 +66,12 @@ app.use((req, res, next) => {
 // routes
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Joanne',
+  });
 });
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);

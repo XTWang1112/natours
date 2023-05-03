fb871cf09b714f75b7a9870015c04e7e8f4a2dc7
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2023-05-03 20:02:43 +1000

    building the login screen

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug })
     .populate({
@@ -29,3 +29,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour: tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
